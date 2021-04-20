b31f5a7b419a9bffb8a2c3626bc8ab1e36808a25
YougTerminator <79044069+YougTerminator@users.noreply.github.com>
2021-04-20 20:42:19 +0530

    Update level.js

--- commands/info/level.js
+++ commands/info/level.js
@@ -33,7 +33,7 @@ const rank = new canvacord.Rank()
     .setUsername(user.username)
     .setDiscriminator(user.discriminator)
     .setRank(1, "a", false)
-    .setBackground("IMAGE", "https://cdn.discordapp.com/attachments/787340979148947466/801140313132367872/PicsArt_01-19-10.54.56.png");
+    .setBackground("IMAGE", "https://cdn.discordapp.com/attachments/827539576892817438/834082411980652615/1613999973833.jpg");
 
 rank.build()
     .then(data => {
